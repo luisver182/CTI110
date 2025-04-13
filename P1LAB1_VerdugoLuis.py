1529eb2072ab84770d98a700c936d3e8da35f159
luisver182 <luisver182@gmail.com>
2025-04-12 23:06:28 -0400

    Add files via upload

---
+++ P1LAB1_VerdugoLuis.py
@@ -0,0 +1,10 @@
+# Verdugo Luis 
+# 04/09/2025
+# P1LAB1
+# first name last name input
+
+print('Enter your first name:', end=' ')
+first_name = input()
+print('Enter your last name:', end=' ')
+last_name = input()
+print('Hello,', first_name, last_name, 'Welcome to CTI-110')
